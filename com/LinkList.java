2f5a65ab26159407d571c439f537ce4081c6c18c
YashPareek1 <yashpareekg4@gmail.com>
2021-05-20 23:08:47 +0530

    commit message

---
+++ com/LinkList.java
@@ -0,0 +1,21 @@
+package com;
+import java.util.LinkedList;
+
+public class LinkList {
+	public void main(String[] args)
+	{
+		 // Creating object of the
+        // class linked list
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+
+        
+        // Adding elements to the linked list
+        ll.add(56);
+        ll.add(30);
+        ll.add(70);
+        System.out.println(ll);
+
+
+    }
+}
+
