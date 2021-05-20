e66517d65c09e5daf5775c242e2e46719c074a5d
YashPareek1 <yashpareekg4@gmail.com>
2021-05-21 00:00:51 +0530

    remove node

--- com/LinkList.java
+++ com/LinkList.java
@@ -14,6 +14,9 @@ public class LinkList {
       ll.add(70);
       ll.add(1 ,30);
       System.out.println(ll);
-
+      
+   // remove elements to the linked list
+      ll.removeFirst();
+      System.out.println(ll);
 }
 }
